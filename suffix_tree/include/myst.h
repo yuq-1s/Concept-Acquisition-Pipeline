cde6ce9770d4a3af92706d989dba6cf66cb4ccd3
yqwang <1119793481@qq.com>
2020-11-13 14:56:30 +0800

    Add: cmake

--- suffix_tree/include/myst.h
+++ suffix_tree/include/myst.h
@@ -1,91 +1,107 @@
 #ifndef __MYST_H
 #define __MYST_H
 
-#include <iostream>
-#include <cstdio>
-#include <string>
 #include <algorithm>
-#include <cstring>
 #include <cassert>
-#include <unordered_map>
-#include <sstream>
-#include <fstream>
 #include <codecvt>
-#include "tqdm-cpp/tqdm.hpp"
+#include <cstdio>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
 
-template<typename CharT>
+template <typename CharT>
 struct NodeT {
-/*
+  /*
 There is no need to create an "Edge" struct.
 Information about the edge is stored right in the node.
 [start_; end) longerval specifies the edge,
 by which the node is connected to its parent node.
 */
 
-    NodeT(long start, long end) : start_(start), end_(end), slink_(nullptr) {}
-    long start_, end_;
-    mutable long leaf_count_ = -1;
-    NodeT* slink_;
-    std::unordered_map<CharT, std::unique_ptr<NodeT>> next;
+  NodeT(long start, long end) : start_(start), end_(end), slink_(nullptr) {}
+  long start_, end_;
+  mutable long leaf_count_ = -1;
+  NodeT *slink_;
+  std::unordered_map<CharT, std::unique_ptr<NodeT>> next;
 
-    long string_length() const {
-        return end_ - start_;
-    }
+  long string_length() const {
+    return end_ - start_;
+  }
 };
 
-template<typename CharT = char> class SuffixTree {
-    std::basic_string<CharT> text_;
-    using Node = NodeT<CharT>;
-    Node* root_;
+template <typename CharT = char>
+class SuffixTree {
+  std::basic_string<CharT> text_;
+  using Node = NodeT<CharT>;
+  Node *root_;
 
-    Node* need_sl_ = root_;
-    long remainder_ = 0;
-    Node* active_node_ = root_;
-    long active_edge_ = 0;
-    long active_len_ = 0;
-    long pos_ = -1;
-    long input_string_length_;
+  Node *need_sl_ = root_;
+  long remainder_ = 0;
+  Node *active_node_ = root_;
+  long active_edge_ = 0;
+  long active_len_ = 0;
+  long pos_ = -1;
+  long input_string_length_;
 
-    CharT active_edge_dge() {
-        return text_[active_edge_];
+  CharT active_edge_dge() {
+    return text_[active_edge_];
+  }
+
+  void add_SL(Node *node) {
+    if (need_sl_ != root_)
+      need_sl_->slink_ = node;
+    need_sl_ = node;
+  }
+
+  bool walk_down(Node *node) {
+    if (long edge_length = std::min(node->end_, pos_ + 1) - node->start_; active_len_ >= edge_length) {
+      active_edge_ += edge_length;
+      active_len_ -= edge_length;
+      active_node_ = node;
+      return true;
     }
+    return false;
+  }
 
-    void add_SL(Node* node) {
-        if (need_sl_ != root_) need_sl_->slink_ = node;
-        need_sl_ = node;
+  std::basic_ostream<CharT> &printBT(std::basic_ostream<CharT> &, const std::basic_string<CharT> &, const Node *, bool) const;
+  std::basic_ostream<CharT> &printBT(std::basic_ostream<CharT> &, const Node *) const;
+  void st_extend(CharT c);
+  const Node *traverse(const CharT *query, const Node *current_node) const;
+  long count_leaf(const Node *node);
+  static std::basic_string<CharT> readFile(const char *filename);
+  void cut_leaf(Node* node, long level) {
+    if (level) {
+      for (auto& pair : node->next) {
+        cut_leaf(pair.second.get(), level-1);
+      }
+    } else {
+      node->next.clear();
     }
+  }
 
-    bool walk_down(Node* node) {
-        if (long edge_length = std::min(node->end_, pos_ + 1) - node->start_; active_len_ >= edge_length) {
-            active_edge_ += edge_length;
-            active_len_ -= edge_length;
-            active_node_ = node;
-            return true;
-        }
-        return false;
-    }
+ public:
+  SuffixTree(const char *filename);
+  ~SuffixTree() {
+    delete root_;
+  }
 
-    std::basic_ostream<CharT>& printBT(std::basic_ostream<CharT>&, const std::basic_string<CharT>&, const Node*, bool) const;
-    std::basic_ostream<CharT>& printBT(std::basic_ostream<CharT>&, const Node*) const;
-    void st_extend(CharT c);
-    const Node* traverse(const CharT* query, const Node* current_node) const;
-    long count_leaf(const Node* node);
-    static std::basic_string<CharT> readFile(const char* filename);
+  std::basic_ostream<CharT> &print(std::basic_ostream<CharT> &os) const {
+    return printBT(os, root_);
+  }
 
-public:
-    SuffixTree(const char* filename);
-    ~SuffixTree() {
-        delete root_;
-    }
+  long count_occurance(const CharT *query) {
+    auto node = traverse(query, root_);
+    return (node ? count_leaf(node) : 0);
+  }
 
-    std::basic_ostream<CharT>& print(std::basic_ostream<CharT>& os) const {
-        return printBT(os, root_);
-    }
-
-    long count_occurance(const CharT* query) {
-        auto node = traverse(query, root_);
-        return (node ? count_leaf(node) : 0);
-    }
+  void freeze(long max_query_level) {
+    assert(root_->leaf_count_ == -1);
+    count_leaf(root_);
+    cut_leaf(root_, max_query_level);
+  }
 };
 
-#endif // __MYST_H
+#endif  // __MYST_H
--- suffix_tree/python/pyst.cc
+++ suffix_tree/python/pyst.cc
@@ -3,8 +3,9 @@
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(suffix_tree, m) {
+PYBIND11_MODULE(pyst, m) {
     py::class_<SuffixTree<wchar_t>>(m, "SuffixTree")
         .def(py::init<const char*>())
-        .def("count_occurance", &SuffixTree<wchar_t>::count_occurance);
+        .def("count_occurance", &SuffixTree<wchar_t>::count_occurance)
+        .def("freeze", &SuffixTree<wchar_t>::freeze);
 }
